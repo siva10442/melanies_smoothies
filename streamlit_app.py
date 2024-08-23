8e736dda06219a8b7ee9bc6388287d4df5c1ae0c
siva10442 <siva10442@gmail.com>
2024-08-23 22:44:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -28,6 +29,10 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Name_on_order)
             values ('""" + ingredients_string + """','""" + Name_On_Order + """')"""
     time_to_insert=st.button("Submit Order")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
     #st.write(my_insert_stmt)
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
