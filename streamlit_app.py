86e025911939604aa771eb1314dbec7ca5233756
siva10442 <siva10442@gmail.com>
2024-08-23 22:55:09 +0530

    requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ if ingredients_list:
     ingredients_string=''
     for fruits_chosen in ingredients_list:
         ingredients_string+= fruits_chosen+' '
+        st.subheader(fruits_chosen+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         #st.text(fruityvice_response.json())
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
