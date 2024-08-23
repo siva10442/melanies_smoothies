85fde6bd72e0821454700e4ecb075558f8c977b5
siva10442 <siva10442@gmail.com>
2024-08-23 22:53:32 +0530

    requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if ingredients_list:
         ingredients_string+= fruits_chosen+' '
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         #st.text(fruityvice_response.json())
-        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Name_on_order)
             values ('""" + ingredients_string + """','""" + Name_On_Order + """')"""
@@ -41,4 +41,3 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-)
