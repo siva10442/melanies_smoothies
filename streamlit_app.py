188b72a40187ecbcd0813b02cd641ee6a59de666
siva10442 <siva10442@gmail.com>
2024-08-23 23:17:11 +0530

    fruits_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ if ingredients_list:
     ingredients_string=''
     for fruits_chosen in ingredients_list:
         ingredients_string+= fruits_chosen+' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
         st.subheader(fruits_chosen+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruits_chosen)
         #st.text(fruityvice_response.json())
