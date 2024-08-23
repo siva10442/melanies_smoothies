d7c1a13a561143272f07dcba20e40e7d74ed17cb
siva10442 <siva10442@gmail.com>
2024-08-23 22:09:00 +0530

    Update streamlit_app.py
    removing active session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,7 +14,7 @@ Name_On_Order=st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:',Name_On_Order)
 
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list=st.multiselect("Choose up to 5 ingrediants", my_dataframe ,max_selections=5)
